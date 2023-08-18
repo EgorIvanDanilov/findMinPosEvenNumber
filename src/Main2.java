190381c750c9825d5be4a81ca13eb89e82454116
Egor Danilov <mellowd94@gmail.com>
2023-08-18 19:15:28 +0200

    fix: evaluate efficiency

--- src/Main2.java
+++ src/Main2.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 import  java.util.List;
 import java.util.ArrayList;
 public class Main2 {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    List<Integer> numbers = new ArrayList<>();
-    while (scanner.hasNextInt()){
-      numbers.add(scanner.nextInt());
+  public static void main(String[] args) {     // Time    Space
+    Scanner scanner = new Scanner(System.in);  // O(1)    O(1)
+    List<Integer> numbers = new ArrayList<>(); // O(1)    O(1)
+    while (scanner.hasNextInt()){              //  n=O(n) O(n)
+      numbers.add(scanner.nextInt());          // *O(1)   *O(1)
     }
-    int min = -1;
-    for (int x : numbers) {
-      if (x % 2 ==0 ) {
-        //если до этого не было четных или новое меньше предфдущего минимума
-        if (x < min || x < min) {
-          min =x;
+    int min = -1;                              // O(1)    O(1)
+    for (int x : numbers) {                    // n=O(n   O(1)
+      if (x % 2 ==0 ) {                        // O(1)
+        //если до этого не было четных или новое меньше предыдущего минимума
+        if (min == -1 || x < min) {            // O(1)
+          min =x;                              // O(1)
         }
       }
     }
-      System.out.println(-1);
+      System.out.println(-1);                  // 0(1)
 
   }
 }
